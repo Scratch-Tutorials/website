c1dc5242944f369dc68cf3be2b326e237ae04992
happex <110610727+ha2pex@users.noreply.github.com>
2023-02-21 20:23:23 +0100

    Commit changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
+  content: [
+    './src/**/*.{html,js}',
+    './*.{html,js}',
+  ],
   theme: {
     extend: {
       fontFamily: {
-        sans: '"Alexandria", sans-serif'
+        sans: ['"Alexandria"', 'sans-serif']
       }
+    },
+    fontWeight: {
+      'extrabold': 800,
+      'black': 900
     },
     colors: {
       'primary': '#1b59ff',
+      'white': '#ffffff'
     }
   },
   plugins: [],
